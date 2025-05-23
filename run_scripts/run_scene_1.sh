1de1e6d97ec5b0e28d97f4d8110bf5cbfe6ee317
Chetan Borse <chetanborse1999@gmail.com>
2025-05-22 18:09:08 -0700

    to run only train.py, commented rest

--- run_scripts/run_scene_1.sh
+++ run_scripts/run_scene_1.sh
@@ -28,11 +28,11 @@ do
     DATA=data/folding_scenes/${SCENE}
     python3 train.py -s ${DATA} --expname "${EXP}" --configs arguments/cloth_splatting/default.py --view_skip 3 --iterations 6000
     # python3 train.py -s ${DATA} --expname "${EXP}" --configs arguments/cloth_splatting/default.py --view_skip 3 --iterations 4000
-    python3 render.py -s ${DATA} --model_path "output/${EXP}" \
-      --configs "arguments/cloth_splatting/default.py"  --meshnet_path "output/${EXP}/meshnet"\
-      --show_flow --log_deform --track_vertices --skip_train --skip_video --iteration ${TEST_ITER} --flow_skip 2
-    python3 scripts/align_eval_trajs.py  --gt_file "${DATA}/trajectory.npz" --traj_file "output/${EXP}/test/ours_${TEST_ITER}/all_trajs.npz"
-    mkdir -p output/${EXPNAME}/tracking/${SCENE}
-    python3 scripts/extract_aligned_trajs.py \
-    --src_dir "output/${EXP}" --target_dir output/${EXPNAME}/tracking/${SCENE} --iteration ${TEST_ITER} --target_name cloth-splatting.npz
+    # python3 render.py -s ${DATA} --model_path "output/${EXP}" \
+    #   --configs "arguments/cloth_splatting/default.py"  --meshnet_path "output/${EXP}/meshnet"\
+    #   --show_flow --log_deform --track_vertices --skip_train --skip_video --iteration ${TEST_ITER} --flow_skip 2
+    # python3 scripts/align_eval_trajs.py  --gt_file "${DATA}/trajectory.npz" --traj_file "output/${EXP}/test/ours_${TEST_ITER}/all_trajs.npz"
+    # mkdir -p output/${EXPNAME}/tracking/${SCENE}
+    # python3 scripts/extract_aligned_trajs.py \
+    # --src_dir "output/${EXP}" --target_dir output/${EXPNAME}/tracking/${SCENE} --iteration ${TEST_ITER} --target_name cloth-splatting.npz
 done
